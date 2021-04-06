5c7e7f45a997bc5e301a097766b0e5fc8ce968a0
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-05 21:56:06 -0500

    completed exercise

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariablesExercise2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Ralph";
+            int userAge = 31;
+            char nickName = 'C';
+            bool isTall = true;
+            double yearBorn = 1989;
+            decimal userWeight = 210.3m;
+
+
+            Console.WriteLine($"My friends name is {userName}, he is {userAge} years old. His nickname is {nickName} and it {isTall}, hes 6 foot. In {yearBorn} he was born and he weighs {userWeight} pounds");
+        } 
+    }
+}
